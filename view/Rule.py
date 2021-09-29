65f0a0c8cdda0d1db614317c7e5a59aaa0e3ca80
Daizman <42967651+Daizman@users.noreply.github.com>
2021-09-29 18:23:44 +0500

    Верстка и тесты окна Rule

--- view/Rule.py
+++ view/Rule.py
@@ -1,9 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
 from PyQt5.QtCore import pyqtSignal
 
 from functools import partial
 
 from view.windows.RuleWindow import UIRuleWindow
 
+from model.Rule import Rule as RuleModel
 from model.Fact import Fact as FactModel
 from controller.Fact import Fact as FactnController
 
@@ -11,4 +15,50 @@ from utils.Mixins import *
 
 
 class Rule(IShowError):
-    pass
+    change_signal = pyqtSignal()
+
+    def __init__(self, rule, parent=None):
+        super(Rule, self).__init__(parent)
+
+        self.ui_name = rule.name
+        self.ui_description = rule.description
+        self.ui_rule = rule
+
+        self.ui = UIRuleWindow()
+        self.ui.setup_ui(self)
+
+        self.refresh_requisite()
+        self.refresh_conclusion()
+
+        self.setup_buttons()
+        self.setup_events()
+
+    def setup_buttons(self):
+        pass
+
+    def setup_events(self):
+        pass
+
+    def refresh_requisite(self):
+        pass
+
+    def refresh_conclusion(self):
+        pass
+
+    def accept_changes(self):
+        pass
+
+    def add_requisite(self):
+        pass
+
+    def add_conclusion(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    _model = RuleModel()
+    view = Rule(_model)
+    view.exec()
+    sys.exit(app.exec_())
+
--- view/windows/RuleWindow.py
+++ view/windows/RuleWindow.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore, QtWidgets
 class UIRuleWindow(object):
     def setup_ui(self, rule_view):
         rule_view.setObjectName('rule_view')
-        rule_view.resize(550, 380)
+        rule_view.resize(650, 380)
 
         rule_view.setWindowTitle('Правило')
 
@@ -18,7 +18,7 @@ class UIRuleWindow(object):
         self.name_le = QtWidgets.QLineEdit()
         self.name_le.setObjectName('name')
 
-        requisite_gb = QtWidgets.QGroupBox('Посылка:')
+        requisite_gb = QtWidgets.QGroupBox('Посылка:', rule_view)
 
         self.requisite_tw = QtWidgets.QTableWidget(requisite_gb)
         self.requisite_tw.setObjectName('requisite_tw')
@@ -36,7 +36,7 @@ class UIRuleWindow(object):
         self.remove_requisite_button = QtWidgets.QPushButton(requisite_gb)
         self.remove_requisite_button.setObjectName('remove_requisite_button')
 
-        conclusion_gb = QtWidgets.QGroupBox('Заключение:')
+        conclusion_gb = QtWidgets.QGroupBox('Заключение:', rule_view)
 
         self.conclusion_tw = QtWidgets.QTableWidget(conclusion_gb)
         self.conclusion_tw.setObjectName('conclusion_tw')
@@ -63,21 +63,36 @@ class UIRuleWindow(object):
         self.button_box = QtWidgets.QDialogButtonBox(buttons)
 
         grid.addWidget(name_l, 1, 0)
-        grid.addWidget(self.name_le, 1, 1, 1, 8)
+        grid.addWidget(self.name_le, 1, 1, 1, 10)
 
-        grid.addWidget(requisite_gb, 2, 0, 1, 4)
-        grid.addWidget(conclusion_gb, 2, 5, 1, 4)
+        grid.addWidget(requisite_gb, 2, 0, 5, 5)
+        grid.addWidget(conclusion_gb, 2, 6, 5, 5)
+
+        grid.addWidget(self.requisite_tw, 5, 0, 1, 5)
+        grid.addWidget(self.conclusion_tw, 5, 6, 1, 5)
+
+        grid.addWidget(self.add_requisite_button, 6, 0)
+        grid.addWidget(self.edit_requisite_button, 6, 2)
+        grid.addWidget(self.remove_requisite_button, 6, 4)
+
+        grid.addWidget(self.add_conclusion_button, 6, 6)
+        grid.addWidget(self.edit_conclusion_button, 6, 8)
+        grid.addWidget(self.remove_conclusion_button, 6, 10)
+
+        grid.addWidget(description_l, 7, 0)
+        grid.addWidget(self.description_le, 8, 0, 1, 11)
+
+        grid.addWidget(self.button_box.buttons()[0], 9, 8)
+        grid.addWidget(self.button_box.buttons()[1], 9, 10)
 
         self.retranslate_ui(rule_view)
 
     def retranslate_ui(self, rule_view):
         _translate = QtCore.QCoreApplication.translate
         rule_view.setWindowTitle(_translate('rule_view', 'Редактирование правила'))
-        self.requisite_tw.setTitle(_translate('EditRuleWindow', 'Посылка'))
         self.add_requisite_button.setText(_translate('EditRuleWindow', 'Добавить'))
         self.edit_requisite_button.setText(_translate('EditRuleWindow', 'Изменить'))
         self.remove_requisite_button.setText(_translate('EditRuleWindow', 'Удалить'))
-        self.conclusion_tw.setTitle(_translate('EditRuleWindow', 'Заключение'))
         self.add_conclusion_button.setText(_translate('EditRuleWindow', 'Добавить'))
         self.edit_conclusion_button.setText(_translate('EditRuleWindow', 'Изменить'))
         self.remove_conclusion_button.setText(_translate('EditRuleWindow', 'Удалить'))
